93d14605096519a8deeb613f4190e865897796a6
Cerdenares <46547568+kainrazl@users.noreply.github.com>
2022-05-29 09:58:39 -0500

    Escenas, cinemáticas y demás

---
+++ Assets/Scenes/Cinematics/Scripts/CinematicSceneManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class CinematicSceneManager : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer videoIntro;
+
+    private void Awake()
+    {
+        videoIntro = GetComponent<VideoPlayer>();
+        videoIntro.Play();
+        videoIntro.loopPointReached += VideoIntro_ended;
+    }
+
+    private void VideoIntro_ended(VideoPlayer source)
+    {
+        MakeSceneTransition();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+            MakeSceneTransition();
+    }
+
+    private void MakeSceneTransition()
+    {
+        if (SceneManager.GetActiveScene().name == "GameIntro")
+            //SceneManager.LoadScene("MainGameScene");
+            Debug.Log("Cargar Escena principal");
+
+        else if (SceneManager.GetActiveScene().name == "MainIntro")
+            SceneManager.LoadScene("MainMenu");
+
+        else if (SceneManager.GetActiveScene().name == "GameCredits")
+            SceneManager.LoadScene("MainMenu");
+
+        else if (SceneManager.GetActiveScene().name == "Victory")
+            SceneManager.LoadScene("GameCredits");
+    }
+}
